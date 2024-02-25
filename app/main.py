ebf82cfc184f92cb04708ec24efcd50e72a458d5
Gwenael Weinstein <gwenael.weinstein@gmail.com>
2024-02-25 17:38:52 +0100

    Add tabs to app

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.title("Docker Streamlit Boilerplate")
+
+tabs = ["Tab 1",
+        "Tab 2"]
+
+tab = st.sidebar.radio("", options = tabs)
+
+st.header(tab)
+
+if tab == tabs[0]:
+    st.write("Tab 1 content")
+
+if tab == tab[1]:
+    st.write("Tab 2 content")
